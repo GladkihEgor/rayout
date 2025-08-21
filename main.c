0ddf85bf669139836d415a515b79602d35cabf0a
GladkihEgor <i@gladkihegor.ru>
2025-08-21 21:43:32 +0500

    add ball bouncing

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <raylib.h>
 
-#define VELOCITY 100
-
 int main()
 {
-	int x = 0;
-	int y = 0;
+	Rectangle ball = {.x=0, .y=0, .width=100, .height=100};
+	float velocity_x = 100;
+	float velocity_y = 100;
 
   InitWindow(800, 600, "rayout");
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
+		int w = GetScreenWidth();
+		int h = GetScreenHeight();
     float dt = GetFrameTime();
-    x += dt * VELOCITY;
-    y += dt * VELOCITY;
+    if (ball.x + ball.width  >= w || ball.x <= 0) velocity_x = -velocity_x;
+    if (ball.y + ball.height >= h || ball.y <= 0) velocity_y = -velocity_y;
+    ball.x += dt * velocity_x;
+    ball.y += dt * velocity_y;
 
 	BeginDrawing();
     ClearBackground(BLACK);
-    DrawRectangle(x, y, 100, 100, RED);
+    DrawRectangleRec(ball, RED);
 	EndDrawing();
   }
 
