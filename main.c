9717f8a4b59db36b6a5c3a594cfc7e1860adbb55
Egor Gladkih <i@gladkihegor.ru>
2025-08-28 22:33:18 +0500

    HOT RELOAD!!!

--- main.c
+++ main.c
@@ -28,12 +28,26 @@ int main()
   InitWindow(WINDOW_W, WINDOW_H, "rayout");
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
+	  if (IsKeyDown(KEY_R)) {
+			if (dlclose(rayout) == -1) TraceLog(LOG_FATAL, "Can't close rayout dll:%s\n", dlerror());
+			rayout = dlopen("librayout.dylib", 0);
+			if (rayout == NULL) TraceLog(LOG_FATAL, "Can't open rayout dll:%s\n", dlerror());
+			init_state = dlsym(rayout, init_state_name);
+			release_state = dlsym(rayout, release_state_name);
+			update_state = dlsym(rayout, update_state_name);
+			render = dlsym(rayout, render_name);
+			if (init_state == NULL) TraceLog(LOG_FATAL, "Can't find %s symbol in rayout dll:%s\n", init_state_name, dlerror());
+			if (release_state == NULL) TraceLog(LOG_FATAL, "Can't find %s symbol in rayout dll:%s\n", release_state_name, dlerror());
+			if (update_state == NULL) TraceLog(LOG_FATAL, "Can't find %s symbol in rayout dll:%s\n", update_state_name, dlerror());
+			if (render == NULL) TraceLog(LOG_FATAL, "Can't find %s symbol in rayout dll:%s\n", render_name, dlerror());
+	  }
+
     float dt = GetFrameTime();
     update_state(&state, dt);
     
-	BeginDrawing();
+		BeginDrawing();
 		render(&state);
-	EndDrawing();
+		EndDrawing();
   }
 
   release_state(&state);
